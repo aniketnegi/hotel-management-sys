7bd82de08eb9db402fe0b2fc3b5f322cc2740507
Aniket Negi <aniket.negi19104@gmail.com>
2022-01-21 00:46:03 +0530

    Implemented the login sys

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-import csv
+from os import name
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'hotel_mgmt'
+app.config['SECRET_KEY'] = 'wTcnxeGX7Uj&@5p$bf%L!QBVx2St!rYUEvtKwF#$nNfs&bbZDraFfNQS2Eib6#t@%@EJ*iqsYugNqpCtJ8d48i@djrZDEeh$U@8sKPkhbu5T&S3jDAfpvP7tmqiis7^$'    
+
+mysql = MySQL(app)
+
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -11,24 +23,25 @@ def login():
         uname = userDetails['username'].strip()
         pwd = userDetails['pwd']
         
-        with open('data.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([uname, pwd])
+        # Fetch user id, pwd from db
+        cursor = mysql.connection.cursor()
+        cursor.execute(f"SELECT * FROM employee_login WHERE username = '{uname}' AND password = '{pwd}'")
+        data = cursor.fetchone()
+        
+        if data:
+            cursor.execute(f"SELECT name FROM employee_login WHERE username = '{uname}' AND password = '{pwd}'")
             
-        with open('creds.csv', 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                if uname == row[0] and pwd == row[1]:
-                    return redirect('/admin')
-                else:
-                    return render_template('login.html', msg="Invalid username or password")
-        
-        
+            for i in cursor.fetchone(): name=i
+            
+            flash(f"Welcome {name}", "success")
+            return redirect(url_for('admin'))
+            cursor.close()
+        else:
+            flash("Invalid username and password")
+            return redirect(url_for('login'))
+    
     return render_template('login.html')
 
-@app.route('/success')
-def success():
-    return render_template('success.html')
 
 @app.route('/admin')
 def admin():
